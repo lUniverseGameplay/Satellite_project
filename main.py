3a764155770c942552d4e6b610241a8fdd5aec5d
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2022-12-04 15:34:32 +0300

    fix_04_12_22

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import math
 
 
 def odefun(x: np.ndarray, t: float):
-    p = np.array([np.array(x[3:6]), np.array([-1 * 6.673 * (10 ** (-11)) * 5.972 * (10 ** 24) * i / ((np.linalg.norm(i)) ** 3) for i in x[0:3]])])
-    return [p[0][0], p[0][1], p[0][2], p[1][0], p[1][1], p[1][2]]
+  p = np.array([np.array(x[3:6]), np.array([-1 * 6.673 * (10 ** (-11)) * 5.972 * (10 ** 24) * i / ((np.linalg.norm(i)) ** 3) for i in x[0:3]])])
+  return [p[0][0], p[0][1], p[0][2], p[1][0], p[1][1], p[1][2]]
+  
+def rotz(gamma):
+  return [[np.cos(gamma), np.sin(gamma) * -1, 0], [np.sin(gamma), np.cos(gamma), 0], [0, 0, 1]]
 
 def get_orbit(r, sol):
     phi = 51.6 * np.pi / 180
@@ -32,31 +35,32 @@ def get_orbit(r, sol):
 
 class Earth():
   def __init__(self, ax):
-    self.G = 6.673 * (10 ** (-11))
-    self.M = 5.972 * (10 ** 24)
-    self.radius = 6371000
+    global G, M, radius
+    G = 6.673 * (10 ** (-11))
+    M = 5.972 * (10 ** 24)
+    radius = 6371000
     self.Oz = [0, 0, 1]
 
-    self.stratosphere_radius = self.radius + 50000
+    self.stratosphere_radius = radius + 50000
     
-    self.draw_me(ax)
-    self.draw_stratosphere(ax)
+    #self.draw_me(ax)
+    #self.draw_stratosphere(ax)
   
   def draw_me(self, ax):
     # Open Image in PIL
     bm = PIL.Image.open('data/earthicefreesm.jpg') 
     bm = np.array(bm.resize([d * 2 // 1 for d in bm.size]))/256
 
-    # Create Earth with texture
+    # Create Earth with texture3
     lons = np.linspace(-180, 180, bm.shape[1]) * np.pi/180 
     lats = np.linspace(-90, 90, bm.shape[0])[::-1] * np.pi/180
 
-    x = self.radius * np.outer(np.cos(lons), np.cos(lats)).T
-    y = self.radius * np.outer(np.sin(lons), np.cos(lats)).T
-    z = self.radius * np.outer(np.ones(np.size(lons)), np.sin(lats)).T
+    x = radius * np.outer(np.cos(lons), np.cos(lats)).T
+    y = radius * np.outer(np.sin(lons), np.cos(lats)).T
+    z = radius * np.outer(np.ones(np.size(lons)), np.sin(lats)).T
 
     # Plot the Earth
-    ax.plot_surface(x, y, z, rstride=4, cstride=4, facecolors = bm)
+    ax.plot_surface(x, y, z, rstride=4, cstride=4, facecolors=bm)
   
 
   def draw_stratosphere(self, ax):
@@ -72,11 +76,11 @@ class Earth():
     ax.plot_wireframe(x_stratosphere, y_stratosphere, z_stratosphere, linewidth=1, alpha=0.3)
 
   def return_data(self):
-    return [self.G, self.M, self.radius, self.stratosphere_radius, self.Oz]
+    return [G, M, radius, self.stratosphere_radius, self.Oz]
 
 
 class Satellite():
-  def __init__(self, ax, E_data):
+  def __init__(self, ax):
     self.mass = 457 * (10 ** 3)
     self.height = 10 ** 6
     self.velocity = 7654
@@ -94,7 +98,7 @@ class Satellite():
 
     self.tau = np.cross(self.orbit_norm, self.init_pos) + [0.5 * i for i in self.orbit_norm]
 
-    r0 = np.asarray([i * (E_data[2] + self.height) for i in self.init_pos])
+    r0 = np.asarray([i * (radius + self.height) for i in self.init_pos])
     v0 = self.tau * self.velocity
 
     tspan = np.linspace(0, 3 * self.time, 10 ** 5)
@@ -104,24 +108,51 @@ class Satellite():
 
     #xd = odeint(odefun, x0, tspan)
 
-    tx = odeint(odefun, x0, tspan)
+    self.x = odeint(odefun, x0, tspan)
+
+    trajectory = [i[0:3] for i in self.x]
+    velocity = [i[3:6] for i in self.x]
+
+    trajectory_corrected = np.zeros(np.shape(trajectory))
+
+    kinetic_enegry = np.zeros(np.shape(trajectory)[0])
+    potential_enegry = np.zeros(np.shape(trajectory)[0])
+
+    file_log = open('data/trajectory_coorected.txt', 'w')
+
+    for i in range(len(tspan)):
+      current_time = tspan[i]
+      #print(current_time)
+      angle_Erth_rotation = -2 * np.pi * current_time / (24 * 3600)
+      #print(angle_Erth_rotation)
+
+      current_point = np.array(trajectory[i][:]).transpose()
+      current_point_corrected = rotz(angle_Erth_rotation) * current_point
+
+      trajectory_corrected[i] = [current_point_corrected[i][i] for i in range(len(current_point_corrected))]
+
+      file_log.write(str(trajectory_corrected[i]))
+      file_log.write('\n')
+
+      kinetic_enegry[i] = 0.5 * self.mass * np.dot(velocity[i], velocity[0])
+      potential_enegry[i] = -1 * G * M * self.mass / np.linalg.norm(current_point)
+      #if i == 2493 or i == 2492 or i == 2494:
+        #print(i, current_time, angle_Erth_rotation, current_point, current_point_corrected, trajectory_corrected[i], sep='\n')
+        #print('---------------------------------------------------------------------')
     
-    print(tx[0])
+    file_log.close()
+    
+    total_energy = potential_enegry + kinetic_enegry
 
-    self.draw_self_orbit(ax, E_data)
+    self.draw_self_orbit(ax, trajectory_corrected)
 
-  def draw_self_orbit(self, ax, E_data):
-    R1 = E_data[2] + self.height
-    T1 = self.time * self.velocity
-    N = 1000.0
+  def draw_self_orbit(self, ax, trajectory_corrected):
+    X = np.array([i[0] for i in trajectory_corrected])
+    Y = np.array([i[1] for i in trajectory_corrected])
+    Z = np.array([i[2] for i in trajectory_corrected])
 
-    t = [T1 * i / N for i in np.arange(0, N, 1)]
-
-    X = np.array([R1 * np.cos(2 * np.pi * w / T1) for w in t])
-    Y = np.array([R1 * np.sin(2 * np.pi * w / T1) for w in t])
-    Z = np.array([w * 0 for w in t])
-
-    ax.plot(X, Y, Z, label='Satellite-orbit', color='r')
+    ax.plot(X, Y, Z, label='Satellite-orbit', color='r', linewidth=1.5)
+    ax.scatter(int(self.x[0][0]), int(self.x[0][1]), int(self.x[0][2]), marker='o', color='k')
     ax.legend()
     
   
@@ -132,12 +163,12 @@ class Satellite():
 
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(projection='3d')
 
 #Satellite_orbit = 0
 
 E = Earth(ax)
-S = Satellite(ax, E.return_data())
+S = Satellite(ax)
 
 #print(S.return_data())
 #print(E.return_data())
